c4e993163dbae7c04356da606adbe80d815cb5a1
Vladimir Komarov <vvkcrtc@gmail.com>
2023-05-22 00:51:33 +0300

    Spring method security task1

--- src/main/java/ru/netology/springjpatask1/configuration/WebSecurityConfig.java
+++ src/main/java/ru/netology/springjpatask1/configuration/WebSecurityConfig.java
@@ -1,21 +1,37 @@
 package ru.netology.springjpatask1.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
 
+@EnableMethodSecurity
+@EnableGlobalAuthentication
 public class WebSecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/all/**").permitAll()
-                .anyRequest()
-                .hasRole("USER")
-                .and()
-                .formLogin();
-        return http.build();
+    public UserDetailsService users() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("READ")
+                .build();
+        UserDetails corr = User.withDefaultPasswordEncoder()
+                .username("corr")
+                .password("corr")
+                .roles("READ","WRITE")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("READ", "WRITE","DELETE")
+                .build();
+        return new InMemoryUserDetailsManager(user, corr, admin);
     }
 }
--- src/main/java/ru/netology/springjpatask1/controller/PersonController.java
+++ src/main/java/ru/netology/springjpatask1/controller/PersonController.java
@@ -1,41 +1,41 @@
 package ru.netology.springjpatask1.controller;
-
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.netology.springjpatask1.repository.Person;
-import ru.netology.springjpatask1.service.PersonService;
-
-import java.util.List;
 
 @RestController
 public class PersonController {
 
-    private final PersonService personService;
 
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
-
-    @GetMapping("/persons/by-city")
-    public List<Person> getPersonsByCity(@RequestParam("city") @Validated String city) {
-        return personService.getPersonsByCity(city);
-    }
-
-    @GetMapping("/persons/by-age")
-    public List<Person> getPersonsByAge(@RequestParam("age") @Validated int age) {
-        return personService.getPersonsByAge(age);
-    }
-
-    @GetMapping("/persons/by-fullname")
-    public Person getPersonsByAge(@RequestParam("name") @Validated String name, @RequestParam("surname") @Validated String surname) {
-        return personService.getPersonsByFullName(name, surname);
-    }
     @GetMapping("/all")
     public String getForAll() {
         return "For all users ...";
+    }
+
+    @GetMapping("/read")
+    @Secured("ROLE_READ")
+    public String getForRead() {
+        return "Role read ...";
+    }
+    @GetMapping("/write")
+    @RolesAllowed("ROLE_WRITE")
+    public String getForWrite() {
+        return "Role write ...";
+    }
+    @GetMapping("/delete")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
+    public String getForDelete() {
+        return "Role write and delete ...";
+    }
+
+    @GetMapping("/info")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String greetingUser(@RequestParam("name") @Validated String username) {
+        return "Hello "+username;
     }
 
 }
